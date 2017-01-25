6dc27dc298b89244c94d1c9e6b493f87b51f137d
ericzhao <ezhao@brainfuse.com>
2017-01-25 11:43:24 -0500

    fix android historyImpl after updating to gwt 2.8

--- src/main/java/com/googlecode/mgwt/mvp/client/history/HistoryImplMGWTAndroid.java
+++ src/main/java/com/googlecode/mgwt/mvp/client/history/HistoryImplMGWTAndroid.java
@@ -15,11 +15,149 @@
  */
 package com.googlecode.mgwt.mvp.client.history;
 
-import com.google.gwt.user.client.impl.HistoryImpl;
+import com.google.gwt.core.client.JavaScriptObject;
+import com.google.gwt.event.logical.shared.HasValueChangeHandlers;
+import com.google.gwt.event.logical.shared.ValueChangeEvent;
+import com.google.gwt.event.logical.shared.ValueChangeHandler;
+import com.google.gwt.event.shared.GwtEvent;
+import com.google.gwt.event.shared.HandlerManager;
+import com.google.gwt.event.shared.HandlerRegistration;
 
-public class HistoryImplMGWTAndroid extends HistoryImpl {
+public class HistoryImplMGWTAndroid implements HasValueChangeHandlers<String> {
 
 	public HistoryImplMGWTAndroid() {
 
 	}
+	
+	private static String token = "";
+
+	  public static String getToken() {
+	    return (token == null) ? "" : token;
+	  }
+
+	  /**
+	   * Sets whether the IE6 history implementation will update the URL hash when
+	   * creating a new item. This should be used only for applications with large
+	   * DOM structures that are suffering from performance problems when creating a
+	   * new history item on IE6 and 7.
+	   * 
+	   * @deprecated This is no longer necessary, as the underlying performance
+	   *             problem has been solved. It is now a no-op.
+	   */
+	  @Deprecated
+	  @SuppressWarnings("unused")
+	  public static void setUpdateHashOnIE6(boolean updateHash) {
+	  }
+
+	  protected static void setToken(String token) {
+		  HistoryImplMGWTAndroid.token = token;
+	  }
+
+	  private JavaScriptObject oldHandler;
+
+	  private HandlerManager handlers = new HandlerManager(null);
+
+	  /**
+	   * Adds a {@link ValueChangeEvent} handler to be informed of changes to the
+	   * browser's history stack.
+	   * 
+	   * @param handler the handler
+	   */
+	  public HandlerRegistration addValueChangeHandler(
+	      ValueChangeHandler<String> handler) {
+	    return handlers.addHandler(ValueChangeEvent.getType(), handler);
+	  }
+
+	  public native void dispose() /*-{
+	    $wnd.onhashchange = this.@com.google.gwt.user.client.impl.HistoryImpl::oldHandler;
+	  }-*/;
+
+	  public native String encodeFragment(String fragment) /*-{
+	    // encodeURI() does *not* encode the '#' character.
+	    return encodeURI(fragment).replace("#", "%23");
+	  }-*/;
+
+	  public void fireEvent(GwtEvent<?> event) {
+	    handlers.fireEvent(event);
+	  }
+
+	  /**
+	   * Fires the {@link ValueChangeEvent} to all handlers with the given tokens.
+	   */
+	  public void fireHistoryChangedImpl(String newToken) {
+	    ValueChangeEvent.fire(this, newToken);
+	  }
+
+	  public HandlerManager getHandlers() {
+	    return handlers;
+	  }
+
+	  public native boolean init() /*-{
+	    var token = '';
+
+	    // Get the initial token from the url's hash component.
+	    var hash = $wnd.location.hash;
+	    if (hash.length > 0) {
+	      token = this.@com.google.gwt.user.client.impl.HistoryImpl::decodeFragment(Ljava/lang/String;)(hash.substring(1));
+	    }
+
+	    @com.google.gwt.user.client.impl.HistoryImpl::setToken(Ljava/lang/String;)(token);
+
+	    var historyImpl = this;
+
+	    historyImpl.@com.google.gwt.user.client.impl.HistoryImpl::oldHandler = $wnd.onhashchange;
+
+	    $wnd.onhashchange = $entry(function() {
+	      var token = '', hash = $wnd.location.hash;
+	      if (hash.length > 0) {
+	        token = historyImpl.@com.google.gwt.user.client.impl.HistoryImpl::decodeFragment(Ljava/lang/String;)(hash.substring(1));
+	      }
+
+	      historyImpl.@com.google.gwt.user.client.impl.HistoryImpl::newItemOnEvent(Ljava/lang/String;)(token);
+	      var oldHandler = historyImpl.@com.google.gwt.user.client.impl.HistoryImpl::oldHandler;
+	      if (oldHandler) {
+	        oldHandler();
+	      }
+	    });
+
+	    return true;
+	  }-*/;
+
+	  public final void newItem(String historyToken, boolean issueEvent) {
+	    historyToken = (historyToken == null) ? "" : historyToken;
+	    if (!historyToken.equals(getToken())) {
+	      setToken(historyToken);
+	      nativeUpdate(historyToken);
+	      if (issueEvent) {
+	        fireHistoryChangedImpl(historyToken);
+	      }
+	    }
+	  }
+
+	  public final void newItemOnEvent(String historyToken) {
+	    historyToken = (historyToken == null) ? "" : historyToken;
+	    if (!historyToken.equals(getToken())) {
+	      setToken(historyToken);
+	      nativeUpdateOnEvent(historyToken);
+	      fireHistoryChangedImpl(historyToken);
+	    }
+	  }
+
+	  protected native String decodeFragment(String encodedFragment) /*-{
+	    // decodeURI() does *not* decode the '#' character.
+	    return decodeURI(encodedFragment.replace("%23", "#"));
+	  }-*/;
+
+	  /**
+	   * The standard updateHash implementation assigns to location.hash() with an
+	   * encoded history token.
+	   */
+	  protected native void nativeUpdate(String historyToken) /*-{
+	    $wnd.location.hash = this.@com.google.gwt.user.client.impl.HistoryImpl::encodeFragment(Ljava/lang/String;)(historyToken);
+	  }-*/;
+
+	  @SuppressWarnings("unused")
+	  protected void nativeUpdateOnEvent(String historyToken) {
+	    // Do nothing, the hash is already updated.
+	  }
 }
