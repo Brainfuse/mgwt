b4395bc06903e3ad64df4b0c162d07b74767de25
ericzhao <ezhao@brainfuse.com>
2018-09-13 14:30:00 -0400

    moved AppHistoryObserver from graphics
    ae55f9d566326d6f854e30dc1af77bccc7fe396e

---
+++ src/main/java/com/googlecode/mgwt/ui/client/brainfuse/AppHistoryObserver.java
@@ -0,0 +1,53 @@
+package com.brainfuse.graphics.client;
+
+
+import com.google.gwt.place.shared.Place;
+import com.google.gwt.user.client.History;
+import com.google.web.bindery.event.shared.EventBus;
+import com.google.web.bindery.event.shared.HandlerRegistration;
+import com.googlecode.mgwt.dom.client.event.mouse.HandlerRegistrationCollection;
+import com.googlecode.mgwt.mvp.client.history.HistoryHandler;
+import com.googlecode.mgwt.mvp.client.history.HistoryObserver;
+
+public class AppHistoryObserver implements HistoryObserver {
+	
+	private HistoryHandler mgwtHistoryHandler;
+
+	public AppHistoryObserver() {}
+	
+	@Override
+	public void onPlaceChange(Place place, HistoryHandler handler) {
+		this.mgwtHistoryHandler = handler;
+	}
+
+	@Override
+	public void onHistoryChanged(Place place, HistoryHandler handler) {
+		this.mgwtHistoryHandler = handler;
+	}
+
+	@Override
+	public void onAppStarted(Place place, HistoryHandler historyHandler) {
+		this.mgwtHistoryHandler = historyHandler;
+	}
+	
+	public HistoryHandler getMgwtHistoryHandler() {
+		return mgwtHistoryHandler;
+	}
+
+	@Override
+	public HandlerRegistration bind(EventBus eventBus, final HistoryHandler historyHandler) {
+		HandlerRegistration register2 = ActionEvent.register(eventBus, ActionEvent.BACK, new ActionEvent.Handler() {
+
+			@Override
+			public void onAction(ActionEvent event) {
+
+				History.back();
+
+			}
+		});
+
+		HandlerRegistrationCollection col = new HandlerRegistrationCollection();
+		col.addHandlerRegistration(register2);
+		return col;
+	}
+}
